0afe9a0a2c075be65423b6bef4c6786ec9b57134
0624028 <u0624028@nkust.edu.tw>
2021-04-13 15:20:28 +0800

    admin

--- datacenter/mysite/views.py
+++ datacenter/mysite/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def index(request):
     MyName = "昇"
+    data = [i for i in range(1,43)]
+    random.shuffle(data)
+    lotto_numbers = data[0:6]
+    special_number = data[28]
+
+
+
+
     return render(request,'index.html',locals())
